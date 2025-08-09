665521d0730c30d4f1b84ec94fa5131d88b37a0d
Leonardo Moya <leonardo.moya1@ulatina.net>
2025-08-09 17:50:37 -0600

    Added the reviews feature

--- vista/misPedidos.php
+++ vista/misPedidos.php
@@ -724,7 +724,7 @@ foreach ($ordenes as &$orden) {
             submitBtn.textContent = 'Enviando...';
             
             // Submit via fetch
-            fetch('../controlador/procesarResena.php', {
+            fetch('../controlador/procesarReseña.php', {
                 method: 'POST',
                 body: formData
             })
